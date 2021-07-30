5e84a1b09d466baac23d401838fd689645d3b252
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-30 22:05:11 +0800

    ok

--- my/ch14stream/createstream/FileToWordsBuilder.java
+++ my/ch14stream/createstream/FileToWordsBuilder.java
@@ -5,20 +5,22 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 // 流的建造者模式
+// forEach() 每接受一个参数不产生结果
+// map()    每接收一个参数产生一个结果
 public class FileToWordsBuilder {
     Stream.Builder<String> builder = Stream.builder();
     public FileToWordsBuilder(String filePath) throws Exception {
         Files.lines(Paths.get(filePath))
             .skip(1)
             .forEach(line -> {
-                for (String w : line.split("[ .?,]+"))
+                for (String w : line.split("[ ,.?]+"))
                     builder.add(w);
             });
     }
     Stream<String> stream() { return builder.build(); }
     public static void main(String[] args) throws Exception {
         new FileToWordsBuilder("/Users/yangwu/vscode-workspace/java-projects/java-base/onjava8-examples/bookcode/streams/Cheese.dat").stream()
-            .limit(17)
+            .limit(7)
             .map(w -> w + " ")
             .forEach(System.out::print);
     }
