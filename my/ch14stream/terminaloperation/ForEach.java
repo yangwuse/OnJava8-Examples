0a2ad9e7c077c64f473b64778533229b5d2b4149
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-31 20:34:05 +0800

    m

--- my/ch14stream/terminaloperation/ForEach.java
+++ my/ch14stream/terminaloperation/ForEach.java
@@ -1,5 +1,9 @@
 package my.ch14stream.terminaloperation;
 
+// forEach 循环
+// parallel() 打乱顺序
+// 加上 forEachOrdered() 还原顺序
+
 public class ForEach {
     static final int SZ = 14;
     public static void main(String[] args) {
@@ -11,7 +15,7 @@ public class ForEach {
             .parallel()
             .forEach(n -> System.out.format("%d ", n));
         System.out.println();
-        
+
         RandInts.rands().limit(SZ)
             .parallel()
             .forEachOrdered(n -> System.out.format("%d ", n));
