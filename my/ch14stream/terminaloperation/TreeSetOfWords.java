0e9b11164a072a4df69ded28f82f63ebfbdb063b
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-31 20:46:25 +0800

    m

---
+++ my/ch14stream/terminaloperation/TreeSetOfWords.java
@@ -0,0 +1,24 @@
+package my.ch14stream.terminaloperation;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class TreeSetOfWords {
+    public static void main(String[] args) throws IOException {
+        Set<String> words = 
+            Files.lines(Paths.get("/Users/yangwu/vscode-workspace/java-projects/java-base/onjava8-examples/my/ch14stream/terminaloperation/TreeSetOfWords.java"))
+                .flatMap(s -> Arrays.stream(s.split("\\W+"))) // 单词
+                .filter(s -> !s.matches("\\d+")) // 非数字
+                .map(String::trim)
+                .filter(s -> s.length() > 2)
+                .limit(100)
+                .collect(Collectors.toCollection(TreeSet::new));
+            System.out.println(words);
+
+    }    
+}
