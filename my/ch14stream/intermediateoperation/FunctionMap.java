b5a8946694ff24d0131239a19fa7f7698e88d459
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-07-31 15:09:58 +0800

    ok

---
+++ my/ch14stream/intermediateoperation/FunctionMap.java
@@ -0,0 +1,36 @@
+package my.ch14stream.intermediateoperation;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+// map(func) 映射函数到输入流中每个元素 
+// 并且将返回值放入输出流中
+public class FunctionMap {
+    static String[] elements = { "12", "", "23", "45" };
+    static Stream<String> testStream() {
+        return Arrays.stream(elements);
+    }
+    static void test(String descr, Function<String, String> func) {
+        System.out.println(" ---( " + descr + " )--- ");
+        testStream()
+            .map(func)
+            .forEach(System.out::println);
+    }
+    public static void main(String[] args) {
+        test("add brackets", s -> "[" + s + "]");
+
+        test("Increment", s -> {
+            try {
+                return Integer.parseInt(s) + 1 + "";
+            } catch(NumberFormatException e) {
+                return s;
+            }
+        });
+
+        test("Replace", s -> s.replace("2", "9"));
+
+        test("Take last digit", s -> s.length() > 0 ? 
+            s.charAt(s.length() - 1) + "" : s);
+    }
+}
