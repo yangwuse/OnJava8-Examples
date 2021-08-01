698eafe9d4d2910c16bb4d737a79d8936b36ff11
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-01 16:31:47 +0800

    m

--- my/ch20generics/genericmethod/GenericVarargs.java
+++ my/ch20generics/genericmethod/GenericVarargs.java
@@ -1,23 +1,22 @@
 package my.ch20generics.genericmethod;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // 泛型方法和变长参数共存
 
 public class GenericVarargs {
-   public static <T> List<T> makeList(T... args) {
+    @SafeVarargs
+    public static <T> List<T> makeList(T... args) {
         return Stream.of(args)
             .collect(Collectors.toList());
-   }
-   public static void main(String[] args) {
-       System.out.println(makeList("A"));
-       System.out.println(makeList("A", "B", "C"));
-       System.out.println(
-           makeList("ABCDEFFHIJKLMNOPQRSTUVWXYZ".split(""))
-       );
-   }
+    }
+    public static void main(String[] args) {
+        System.out.println(makeList("A"));
+        System.out.println(makeList("A", "B", "C"));
+        System.out.println(
+            makeList("ABCDEFFHIJKLMNOPQRSTUVWXYZ".split(""))
+        );
+    }
 }
