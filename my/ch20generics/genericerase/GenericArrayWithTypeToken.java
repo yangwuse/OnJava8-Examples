9d7c184d8eda4a257f92ddfc0239ca18fc818975
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-02 16:14:53 +0800

    m

---
+++ my/ch20generics/genericerase/GenericArrayWithTypeToken.java
@@ -0,0 +1,23 @@
+package my.ch20generics.genericerase;
+
+import java.lang.reflect.Array;
+
+// 传入类型标记
+
+public class GenericArrayWithTypeToken<T> {
+    private T[] array;
+    @SuppressWarnings("unchecked")
+    public GenericArrayWithTypeToken(Class<T> type, int sz) {
+        array = (T[]) Array.newInstance(type, sz);
+    }
+    public void put(int i, T e) { array[i] = e; }
+    public T get(int i) { return array[i]; }
+    public T[] rep() { return array; }
+    public static void main(String[] args) {
+        GenericArrayWithTypeToken<Integer> gai = 
+            new GenericArrayWithTypeToken<>(Integer.class, 10);
+        // ok
+        Integer[] ia = gai.rep();
+    }
+
+}
