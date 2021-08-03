a2f806212eb4eebe8e7844c11fdc6a36583e303d
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-03 13:52:47 +0800

    m

--- my/ch20generics/wildcard/GenericReading.java
+++ my/ch20generics/wildcard/GenericReading.java
@@ -7,16 +7,17 @@ import java.util.List;
 public class GenericReading {
     static List<Apple> apples =  Arrays.asList(new Apple());
     static List<Fruit> fruit = Arrays.asList(new Fruit());
+    // 使用精确类型(无通配符)
     static <T> T readExact(List<T> list) {
         return list.get(0);
     }
-
     static void f1() {
         Apple a = readExact(apples);
         Fruit f = readExact(fruit);
         f = readExact(apples);
     }
 
+    // 泛型类在实例化确定唯一类型
     static class Reader<T> {
         T readExact(List<T> list) { return list.get(0); }
     }
@@ -29,6 +30,7 @@ public class GenericReading {
     }
 
     static class CovariantReader<T> {
+        // 从 List<? extends t> 中读取一个 T 
         T readCovariant(List<? extends T> list) {
             return list.get(0);
         }
