57f33886077e59e84085805a972160d32e097e4d
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-05 17:25:40 +0800

    m

---
+++ my/functionnal/highorderfunction/TransformFunction.java
@@ -0,0 +1,29 @@
+package my.functionnal.highorderfunction;
+
+import java.util.function.Function;
+
+// 基于消费函数来生产函数的函数
+
+class I {
+    @Override public String toString() { return "I"; }
+}
+
+class O {
+    @Override public String toString() { return "O"; }
+}
+
+public class TransformFunction {
+    static Function<I, O> transform(Function<I, O> in) {
+        return in.andThen(o -> {
+            System.out.println(o);
+            return o;
+        });
+    }
+    public static void main(String[] args) {
+        Function<I, O> f2 = transform(i -> {
+            System.out.println(i);
+            return new O();
+        });
+        O o = f2.apply(new I());
+    }
+}
