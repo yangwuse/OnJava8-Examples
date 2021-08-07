ba30b455807ca6aae6dbbe42ed993cd25ec3e957
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-08-07 15:55:37 +0800

    m

--- my/interfaces/decouple/Applicator.java
+++ my/interfaces/decouple/Applicator.java
@@ -2,21 +2,26 @@ package my.interfaces.decouple;
 
 import java.util.Arrays;
 
+// 策略模式 
+// 子类对象作为策略传入方法
+// UpCase DownCase Splitter 对象作为策略
+
 class Processor {
     String name() {
         return getClass().getSimpleName();
     }
-    Object process(Object input) { return input; }
+    Object process(Object input) {
+        return input;
+    }
 }
 
-class Upcase extends Processor {
-    // 返回协变类型
+class UpCase extends Processor {
     @Override String process(Object input) {
         return ((String)input).toUpperCase();
     }
 }
 
-class Downcase extends Processor {
+class DownCase extends Processor {
     @Override String process(Object input) {
         return ((String)input).toLowerCase();
     }
@@ -28,16 +33,18 @@ class Splitter extends Processor {
     }
 }
 
+
+
 public class Applicator {
-    static void apply(Processor p, Object s) {
-        System.out.println("Using processor " + p.name());
+    // 使用策略 父类型作为接受类型
+    public static void apply(Processor p, Object s) {
+        System.out.println("Using Processor " + p.name());
         System.out.println(p.process(s));
     }
     public static void main(String[] args) {
-        String s =
-        "We are such stuff as dreams are made on";
-        apply(new Upcase(), s);
-        apply(new Downcase(), s);
+        String s = "Hello World";
+        apply(new UpCase(), s);
+        apply(new DownCase(), s);
         apply(new Splitter(), s);
     }
 }
